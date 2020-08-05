9fa6a7309433ea198154eb1bb71ac1b69e907f22
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2020-08-05 14:46:23 +0200

    feat: Add Web Pack Html Plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path")
 const webpack = require("webpack")
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+
 module.exports = {
     entry: './src/client/index.js',
     module: {
@@ -10,5 +12,11 @@ module.exports = {
                 loader: "babel-loader"
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        })
+    ]
 }
